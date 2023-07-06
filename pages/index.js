54ac1c255db888df837e3df3a43ea51365791b4a
Elio <100736466+Eliomatheus@users.noreply.github.com>
2023-07-06 14:39:58 +0000

    Adcionando a Pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home() {
+  return <h1>Penso em você do momento que acordo até a hora que vou dormir. Eu te amo, Emily Portela ❤️.<br/>
+  Se vc me ama da uma risadinha bb ❤️ </h1>
+}
+
+export default Home;
