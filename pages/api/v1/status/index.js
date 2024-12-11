f2ca5e20ecf8a459b0511ab18c4abb7de55f53b4
Elio <100736466+Eliomatheus@users.noreply.github.com>
2024-12-11 00:42:27 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,12 @@
-function status(request, response) {
-  response
-    .status(200)
-    .json({
-      Frase_motivacional:
-        "Tentando ser 1% melhor a cada dia, a competição é comigo mesmo...",
-    });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows);
+  response.status(200).json({
+    Frase_motivacional:
+      "Tentando ser 1% melhor a cada dia, a competição é comigo mesmo...",
+  });
 }
 
 export default status;
